5cd3936bb296a91a35d122fd76bab2bae04169d5
Sandeep Madhiri <sandeep_madhiri@persistent.com>
2022-05-26 14:23:50 +0530

    Added cros support

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var app = express();
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+    res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
+    next();
+})
+
 var  customerApi = require("./controller/customer.controller")
 app.use("/api/customer",customerApi);
 
